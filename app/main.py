321deca7f6d600f2db924be506adb175e3168298
KG <krightangle@gmail.com>
2025-07-26 12:46:14 -0400

    dockerized, two endpoints

--- app/main.py
+++ app/main.py
@@ -11,10 +11,33 @@ class Note(BaseModel):
 	username:str
 	note: str
 
-external_input = {
-	"username": "kgadgil",
-	"note": "Hello World",
-}
+## Test out Note model with external input
+## $ python3 app/main.py 
+## username='kgadgil' note='Hello World'
+# external_input = {
+# 	"username": "kgadgil",
+# 	"note": "Hello World",
+# }
 
-note = Note(**external_input)
-print(note)
+# note = Note(**external_input)
+# print(note)
+
+## How do decorated functions work
+## https://stackoverflow.com/questions/46123448/how-do-decorated-functions-work-in-flask-python-app-route
+## decorator adds endpoint to app object declared above
+## does the equivalent of calling the following function as follows
+## app.add_url_rule("/", "notes", add_note)
+## Decorators are functions that wrap other functions
+@app.post("/notes")
+def add_note(note: Note):
+	if note.username not in db:
+		db[note.username] = []
+	db[note.username].append(note.note)
+	return {"message": "Note added successfully"}
+
+@app.get("/notes/{username}")
+def get_notes(username: str):
+	if username not in db:
+		raise HTTPException(status_code=404, detail="User not found")
+	return {"username":username, "notes": db[username]}
+
--- doc/DEV_HELPER.md
+++ doc/DEV_HELPER.md
@@ -9,3 +9,18 @@ git config --global alias.p 'push'
 git config --global alias.ci commit
 git config --global --unset alias.ci
 git config --global alias.cm 'commit -m'
+
+docker compose up --build
+
+http://localhost:8080/docs
+
+https://stackoverflow.com/questions/572549/difference-between-git-add-a-and-git-add
+
+
+git add -A stages all changes
+
+git add . stages new files and modifications, without deletions (on the current directory and its subdirectories).
+
+git add -u stages modifications and deletions, without new files
+
+echo "app/__pycache__/"$'\n' >> .gitignore
